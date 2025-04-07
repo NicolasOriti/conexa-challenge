cde4412a4f85dcaf6c54c130a12d2eff7a69e36c
Nicolas Oriti Tizio <nicolasfranco.oriti@naranjax.com>
2025-04-07 13:15:49 -0300

    feat: integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Conexa API')
+    .setDescription('API para gestión de películas y usuarios')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // quita propiedades no definidas en DTO
-      forbidNonWhitelisted: true, // lanza error si vienen propiedades no permitidas
-      transform: true, // transforma los tipos (por ejemplo string → number)
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
